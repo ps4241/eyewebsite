2225782e1df9959374c32ce01724ceea22c79bbb
prasad supekar <supekarprasad3@gmail.com>
2025-04-17 16:29:48 +0530

    Update connection.js

--- eyewebsite/connection.js
+++ eyewebsite/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var  util = require("util");
 var conn = mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"eyehospital"
+    "host":"bx1vaxcq0en7l5bwgsxl-mysql.services.clever-cloud.com",
+    "user":"ubdlmkybsf8b9y2f",
+    "password":"OLUnv1xJD8LuWkLdaCaO",
+    "database":"bx1vaxcq0en7l5bwgsxl"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
